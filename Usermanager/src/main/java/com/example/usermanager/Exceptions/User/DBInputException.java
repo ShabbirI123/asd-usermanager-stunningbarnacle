1fe17692e767c748e02e39ebda7fd01ea7323dbf
Shabbir <shabbirislam801@gmail,com>
2022-11-25 01:38:03 +0100

    deleted ErrorMessage.java file and modified the DBInputException.java file

--- Usermanager/src/main/java/com/example/usermanager/Exceptions/User/DBInputException.java
+++ Usermanager/src/main/java/com/example/usermanager/Exceptions/User/DBInputException.java
@@ -2,20 +2,8 @@ package com.example.usermanager.Exceptions.User;
 
 public class DBInputException extends Exception{
 
-    public DBInputException(String errorMessage, String errorMessageCode, Throwable throwable){
-        super(DBInputException.getExceptionText(errorMessage, errorMessageCode), throwable);
+    public DBInputException(String errorMessage, Throwable throwable){
+        super(errorMessage, throwable);
     }
 
-    public static String getExceptionText(String errorMessage, String errorMessageCode) {
-        if (errorMessage == null || errorMessage == ""){
-            for (ErrorMessage message : ErrorMessage.values()){
-                if (ErrorMessage.valueOf(errorMessageCode) == message){
-                    return message.getCode();
-                }
-            }
-        } else {
-            return errorMessage;
-        }
-        return "";
-    }
 }
