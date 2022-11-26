6903a074663e8d1315816ff428d41a38d99a17ad
iniyininggao <ini6500964@gmail.com>
2022-11-26 12:29:49 +0100

    spring boot security files were added

---
+++ Usermanager/src/main/java/com/example/usermanager/security/UserSecurityService.java
@@ -0,0 +1,42 @@
+package com.example.usermanager.security;
+
+import com.example.usermanager.model.User;
+import com.example.usermanager.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserSecurityService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null){
+            return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthorities());
+        }
+        return null;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> list = new ArrayList<GrantedAuthority>();
+        list.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return list;
+    }
+}
