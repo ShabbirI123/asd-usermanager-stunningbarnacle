b62f9b1ba53a3881e0444cd2a1a7fef074603901
Elna Halilovic <elna.halilovic@tailored-apps.com>
2022-12-03 21:31:14 +0100

    add register screen and change user screen

--- Usermanager/src/main/resources/static/js/index.js
+++ Usermanager/src/main/resources/static/js/index.js
@@ -1,5 +1,60 @@
-function afterInactivityRedirect(){
-    alert('Redirect');
-    location.reload();
-}
-setTimeout(afterInactivityRedirect, 60000);
+$(document).ready(function () {
+    //Simple client based login limitation by using localstorage
+   if (location.href === "http://localhost:8080/login?error"){
+       if (!localStorage.getItem('limit')){
+           localStorage.setItem('limit', "0");
+       } else {
+           let limit = parseInt(localStorage.getItem('limit'));
+           console.log(limit);
+           console.log(limit++)
+           localStorage.setItem('limit', limit.toString());
+       }
+
+       if(localStorage.getItem('limit') === '3'){
+           $('#login-btn').css('pointer-events', 'none');
+       }
+   }
+
+
+    function afterInactivityRedirect() {
+        alert('Redirect');
+        location.reload();
+    }
+
+    //setTimeout(afterInactivityRedirect, 60000);
+
+    //Restriction of login attempts
+    $('#login-btn').on('click',function (){
+        let username = $('#username').val();
+        let password = $('#password').val();
+
+        console.log('click');
+
+        var settings = {
+            "url": "http://localhost:8080/login",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+        };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+
+
+       if(localStorage.getItem('limit') === '3'){
+           $(this).css('pointer-events', 'none');
+       }
+    });
+
+    $('#logout-btn').on('click', () => {
+        localStorage.setItem('limit', "0");
+    })
+});
+
