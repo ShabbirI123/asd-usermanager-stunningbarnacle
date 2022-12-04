03e5b79ba34bb12c3fa60b72afb66af73de5fa5e
Shabbir <shabbirislam801@gmail,com>
2022-12-04 17:02:58 +0100

    added message

---
+++ Usermanager/src/main/resources/static/js/ajax.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+    //Create account
+    $('#register-btn').on('click', function () {
+        let firstname = $('#register #firstName').val();
+        let lastname = $('#register #lastName').val();
+        let username = $('#register #username').val();
+        let password = $('#register #password').val();
+
+        console.log(firstname + lastname + username + password);
+        if (!firstname || !lastname || !username || !password) {
+            return;
+        }
+
+        let settings = {
+            "url": "http://localhost:8080/api/v1/user/register/",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+                "firstname": firstname,
+                "surname": lastname,
+                "username": username,
+                "password": password
+            }),
+        };
+
+        $.ajax(settings)
+            .done(function (response) {
+                location = "http://localhost:8080/register?success";
+            })
+            .fail(function (jqXHR, textStatus) {
+                location = "http://localhost:8080/register?error";
+            });
+    });
+
+    //Change password
+    $('#change-password-btn').on('click', function () {
+        let id = $('#current-password').attr('data-userID');
+        let currentPassword = $('#current-password').val();
+        let newPassword = $('#new-password').val();
+        let secondNewPassword = $('#2nd-new-password').val()
+
+        if (!id || !currentPassword || !newPassword) {
+            return;
+        }
+
+        if (newPassword !== secondNewPassword) {
+            $('#pwd-msg').html('Passwörter stimmen nicht überein.')
+            return;
+        }
+
+        let settings = {
+            "url": "http://localhost:8080/api/v1/user/edit/",
+            "method": "PUT",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+                "id": id,
+                "currentPassword": currentPassword,
+                "newPassword": newPassword
+            }),
+        };
+
+        $.ajax(settings)
+            .done(function (response) {
+                $('#pwd-msg').html('Passwort geändert.')
+            })
+            .fail(function () {
+                $('#pwd-msg').html('Passwort ist nicht korrekt.')
+            });
+    });
+    //Delete account
+    $('#delete-account-btn').on('click', function () {
+        let id = $('#password').attr('data-userID');
+        let currentPassword = $('#password').val();
+
+        console.log(id + currentPassword);
+        if (!id || !currentPassword) {
+            return;
+        }
+        let settings = {
+            "url": "http://localhost:8080/api/v1/user/delete/",
+            "method": "DELETE",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+                "id": id,
+                "currentPassword": currentPassword
+            }),
+        };
+
+        $.ajax(settings)
+            .done(function (response) {
+                window.location = "http://localhost:8080/logout"
+            })
+            .fail(function () {
+                $('#delete-msg').html('Passwort ist nicht korrekt.')
+            });
+
+    });
+});
+
--- Usermanager/src/main/resources/static/js/index.js
+++ Usermanager/src/main/resources/static/js/index.js
@@ -1,5 +1,72 @@
-function afterInactivityRedirect(){
-    alert('Redirect');
-    location.reload();
-}
-setTimeout(afterInactivityRedirect, 60000);
+$(document).ready(function () {
+    //Simple client based login limitation by using localstorage
+    if (localStorage.getItem('limit') >= '2') {
+        //set date
+        let t = new Date();
+        t.setSeconds(t.getSeconds() + 60);
+        if (!localStorage.getItem('date')) localStorage.setItem('date', t.toString());
+
+        if (new Date(localStorage.getItem('date')) > new Date()) {
+            //disable submit btn
+            $('#login-btn').css('pointer-events', 'none');
+            //Message
+            $('#blocked-msg').html("Benutzer bis zum " + localStorage.getItem('date') + " Uhr gesperrt");
+        } else {
+            localStorage.setItem('limit', '0');
+            localStorage.removeItem('date');
+        }
+    }
+
+    if (location.href === "http://localhost:8080/login?error") {
+        if (!localStorage.getItem('limit')) {
+            localStorage.setItem('limit', "0");
+        } else {
+            let limit = parseInt(localStorage.getItem('limit'));
+            if (limit < 2) limit++;
+            localStorage.setItem('limit', limit.toString());
+        }
+    }
+
+
+    function afterInactivityRedirect() {
+        alert('Redirect');
+        location.reload();
+    }
+
+    //setTimeout(afterInactivityRedirect, 60000);
+
+    //Restriction of login attempts
+    $('#login-btn').on('click', function () {
+        let username = $('#username').val();
+        let password = $('#password').val();
+
+        console.log('click');
+
+        var settings = {
+            "url": "http://localhost:8080/login",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "data": JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+        };
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+
+
+        if (localStorage.getItem('limit') === '3') {
+            $(this).css('pointer-events', 'none');
+        }
+    });
+
+    $('#logout-btn').on('click', () => {
+        localStorage.setItem('limit', "0");
+    })
+});
+
